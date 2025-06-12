f862cd24d330652d7b18c5c68f5f73e7931140dd
juan arroyo <jearroyo27@ucatolica.edu.co>
2025-06-12 04:26:09 -0500

    Creada la conexion igual revisa a ver si ya esta funcional

--- users.py
+++ users.py
@@ -28,4 +28,21 @@ class UserCreate(BaseModel):
     class Config:
         orm_mode = True
 
+
+class Pet(BaseModel):
+    name:str = Field(..., min_length=3, max_length=20)
+    breed: Optional[str] = Field(None, min_length=3, max_length=25)  # Opcional
+    birth: Optional[int] = Field(None, gt=2000, lt=2025)  # Opcional
+    kind: Optional[str] = Field(None, min_length=3, max_length=25)  # Opcional
+    female: Optional[bool] = Field(None)  # Opcional
+
+class PetResponse(BaseModel):
+    name:str
+    kind:str
+
+
+
+class UpdatedPet(BaseModel):
+    name: Optional[str] = Field(..., min_length=3, max_length=20)
+    breed:Optional[str] = Field(..., min_length=3, max_length=25)
 #token: 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
